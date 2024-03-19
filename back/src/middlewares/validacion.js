f1843dbded9db48d07e5a99c60f416e39bfa8b42
marcos <marcosgomez48149@gmail.com>
2024-03-19 18:38:08 -0300

    modificando 19/03

--- back/src/middlewares/validacion.js
+++ back/src/middlewares/validacion.js
@@ -2,7 +2,8 @@ const validateMovies = (req, res, next) => {
 
     const { title, year, director, duration,genre,rate, poster } = req.body
 
-    if(![ title, year, director, duration,genre,rate, poster ].every(Boolean)) return res.status(400).json({message: "Faltan datos por completar"})
+    if(![ title, year, director, duration,genre,rate, poster ].every(Boolean)) 
+    return res.status(400).json({message: "Faltan datos por completar"})
 
     next()
 
